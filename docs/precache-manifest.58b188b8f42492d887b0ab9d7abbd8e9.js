67cd869d0316557e4b215cee45c654ef5ce8e4f5
Ricard Lafuente <dev2@pegaso.es>
2022-03-23 16:26:37 +0100

    Docs agregado

---
+++ docs/precache-manifest.58b188b8f42492d887b0ab9d7abbd8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5446902043a13b3d85e15382ecf94229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6d5a0261df2c73132c5",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d5a0261df2c73132c5",
+    "url": "/static/js/main.dfca3727.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
